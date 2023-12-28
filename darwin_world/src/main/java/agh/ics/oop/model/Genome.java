d3e079cb1fe2aadae2596d33192ecb9b7f06e53f
Jeremylaby <barycki@student.agh.edu.pl>
2023-12-28 16:42:29 +0100

    genome raczej done

--- darwin_world/src/main/java/agh/ics/oop/model/Genome.java
+++ darwin_world/src/main/java/agh/ics/oop/model/Genome.java
@@ -8,16 +8,20 @@ import java.util.List;
 public class Genome {
     private List<Integer> genes;
     private int currentGeneIndex=0;
+    private boolean isalternative;
+    private boolean flag;
 
 
 
-    public Genome(int n){
+    public Genome(int n,boolean isalternative){
         RandomNumGenerator randomNumGenerator=new RandomNumGenerator(0,7);
         List<Integer> list = randomNumGenerator.generateRandomIntList(n);
         this.genes=list;
+        this.isalternative=isalternative;
     }
     public Genome(List<Integer> genes,int min,int max) {
         this.genes=genes;
+        this.isalternative=isalternative;
         mutate(min,max);
     }
     public List<Integer> getGenes() {
@@ -30,6 +34,23 @@ public class Genome {
     private void setGenes(List<Integer> genes) {
         this.genes = genes;
     }
+    public int getCurentGene(){
+        int ind=this.currentGeneIndex;
+        if(isalternative){
+            nextGeneBackAndForth();
+        }else{
+            nextGene();
+        }
+        return genes.get(ind);
+
+    }
+    public List<Integer> getRightGenesSlice(int n){
+        return new ArrayList<>(genes.subList( genes.size()-n,genes.size()));
+    }
+    public List<Integer> getLeftGenesSlice(int n){
+        return new ArrayList<>(genes.subList(0,n));
+    }
+
     private void mutate(int min,int max){
         List<Integer> newGenes=new ArrayList<>(this.getGenes());
         RandomNumGenerator randomNumGenerator=new RandomNumGenerator(min,max);
@@ -44,6 +65,23 @@ public class Genome {
         });
         this.setGenes(newGenes);
     }
+    private void nextGene(){
+        currentGeneIndex+=1%genes.size();
+    }
+    private void nextGeneBackAndForth(){
+        if(flag){
+            currentGeneIndex+=1;
+            if(currentGeneIndex==genes.size()-1){
+                flag=false;
+            }
+        }else{
+            currentGeneIndex-=1;
+            if(currentGeneIndex==0){
+                flag=true;
+            }
+        }
+    }
+
 
     @Override
     public String toString() {
